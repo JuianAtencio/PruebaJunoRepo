8b28650f59b89d1c90cbbe9cc0cdce73e6568908
julian <julian@julian-PC>
2013-05-13 12:46:14 -0500

    SE SUBE AL HUB

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
 	 public static void main(String[] parametro) {
-	        System.out.println("=============Aja Programa de MODIFICADO DOS prueba para GIT============");  
+	        System.out.println("=============Aja Programa de MODIFICADO DOS prueba para GIT CON hub============");  
 	         Imprimir imprimir= new Imprimir();
 	         imprimir.devuelve_mensaje(true);
 	         //imprimir.devuelve_mensaje(false);  
