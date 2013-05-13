b99cdabe1c43d9283581bec267ed33b76f3c5d10
julian <julian@julian-PC>
2013-05-13 14:04:47 -0500

    se da formato a las clases

--- src/Imprimir.java
+++ src/Imprimir.java
@@ -1,25 +1,26 @@
-
 public class Imprimir {
-	public boolean devuelve_mensaje(boolean dato){
-		if(dato != true){
-			 System.out.println("Aja programa de prueba para repo en Git  falso");
-			 dato = false;
-		}else{
-			 System.out.println("Aja programa de prueba para repo en Git true");
-			 dato = true;
-		}
-		return dato;
-	}
-	
-	public boolean devuelve_mensaje2(boolean dato){
-		if(dato != true){
-			 System.out.println("Aja 2 MNS programa de prueba para repo en Git  falso");
-			 dato = false;
-		}else{
-			 System.out.println("Aja 2 MNS programa de prueba para repo en Git true");
-			 dato = true;
-		}
-		return dato;
-	}
-}
+    public boolean devuelve_mensaje(boolean dato) {
+        if (dato != true) {
+            System.out
+                    .println("Aja programa de prueba para repo en Git  falso");
+            dato = false;
+        } else {
+            System.out.println("Aja programa de prueba para repo en Git true");
+            dato = true;
+        }
+        return dato;
+    }
 
+    public boolean devuelve_mensaje2(boolean dato) {
+        if (dato != true) {
+            System.out
+                    .println("Aja 2 MNS programa de prueba para repo en Git  falso");
+            dato = false;
+        } else {
+            System.out
+                    .println("Aja 2 MNS programa de prueba para repo en Git true");
+            dato = true;
+        }
+        return dato;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
-
 public class Main {
-	 public static void main(String[] parametro) {
-	        System.out.println("=============Aja Programa de MODIFICADO DOS prueba para GIT CON hub============");  
-	         Imprimir imprimir= new Imprimir();
-	         imprimir.devuelve_mensaje(true);
-	         //imprimir.devuelve_mensaje(false);  
-	 }	 
+    public static void main(String[] parametro) {
+        System.out
+                .println("=============Aja Programa de MODIFICADO DOS prueba para GIT CON hub============");
+        Imprimir imprimir = new Imprimir();
+        imprimir.devuelve_mensaje(true);
+        // imprimir.devuelve_mensaje(false);
+    }
 }
