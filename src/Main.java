bf53a8d8ae13a4b21b647b28cacd34277b964ab7
julian <julian@julian-PC>
2013-05-13 12:44:02 -0500

    OTRO CAMBIO

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
 	 public static void main(String[] parametro) {
-	        System.out.println("=============Aja Programa de MODIFICADO prueba para GIT============");  
+	        System.out.println("=============Aja Programa de MODIFICADO DOS prueba para GIT============");  
 	         Imprimir imprimir= new Imprimir();
 	         imprimir.devuelve_mensaje(true);
 	         //imprimir.devuelve_mensaje(false);  
