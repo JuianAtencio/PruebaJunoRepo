2accb7c65d8feb143aca1a56341baf501922a20d
julian <julian@julian-PC>
2013-05-13 13:12:54 -0500

    se realizan algunos cambios

--- src/Imprimir.java
+++ src/Imprimir.java
@@ -10,4 +10,16 @@ public class Imprimir {
 		}
 		return dato;
 	}
+	
+	public boolean devuelve_mensaje2(boolean dato){
+		if(dato != true){
+			 System.out.println("Aja 2 MNS programa de prueba para repo en Git  falso");
+			 dato = false;
+		}else{
+			 System.out.println("Aja 2 MNS programa de prueba para repo en Git true");
+			 dato = true;
+		}
+		return dato;
+	}
 }
+
--- src/Main.java
+++ src/Main.java
@@ -7,4 +7,3 @@ public class Main {
 	         //imprimir.devuelve_mensaje(false);  
 	 }	 
 }
-
