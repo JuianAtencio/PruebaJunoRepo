cc6f8ee029c30a752da70617db46e41aa7c2d3dd
julian <julian@julian-PC>
2013-05-13 12:34:16 -0500

    Se actualiza la información

---
+++ src/Imprimir.java
@@ -0,0 +1,13 @@
+
+public class Imprimir {
+	public boolean devuelve_mensaje(boolean dato){
+		if(dato != true){
+			 System.out.println("Aja programa de prueba para repo en Git  falso");
+			 dato = false;
+		}else{
+			 System.out.println("Aja programa de prueba para repo en Git true");
+			 dato = true;
+		}
+		return dato;
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	 public static void main(String[] parametro) {
+	        System.out.println("=============Aja Programa de prueba para GIT============");  
+	         Imprimir imprimir= new Imprimir();
+	         imprimir.devuelve_mensaje(true);
+	         //imprimir.devuelve_mensaje(false);  
+	 }	 
+}
+
